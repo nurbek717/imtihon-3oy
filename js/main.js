a0a0c61c4fb6dff17b0101678091d02ed6a3f13d
nurbek717 <nurbektukhtamishev@gmail.com>
2021-11-20 18:08:35 +0500

    salom

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
-
-// for (let i = 0; i < 20; i++) {
-//      console.log("salom");
+/////// 1-masiq ___________________________________________________
 
 
-//      var inCard = document.createAttribute("div");
-//      var inImg = document.createAttribute("img");
-//      var inName = document.createAttribute("h2");
-//      var inTayp = document.createAttribute("p");
 
 
-// elCardImg.innerHTML = `p`
-// } 
 
 
+
+// /  2 - mashiq __________________________________________________
+
+var arryMetodic = ["jazz"," blues"]
+console.log(arryMetodic);
+
+arryMetodic.push("rock-n-rol")
+console.log(arryMetodic);
+
+arryMetodic.splice(1,1, "classic")
+console.log(arryMetodic);
+
+arryMetodic.splice(0,1, )
+console.log(arryMetodic);
+
+arryMetodic.unshift("rap", "reggae")
+console.log(arryMetodic);
+
+
+function upText(ismlar) {
+ var resalt = []
+ 
+ for (var name of ismlar) {
+  var slice = name.split("")
+  slice[0] = slice[0].toUpperCase()
+  resalt.push(slice.join(""))
+  
+ }
+ console.log(resalt);
+}
+
+console.log(upText(arryMetodic));
+
+
+///Pokemons________________________________________________________
 var pokemons = [
 	{
 		"name": "Bulbasaur",
@@ -203,3 +230,21 @@ console.log(elCardImg);
 // .classList.add("list__item");
 
 
+/// 3-mashiq  _____________________________________________________
+
+
+var elText = document.querySelector(".text")
+var salaries = {
+	"Akmal": 1200,
+	"Salim": 5200,
+	"Karima": 1800,
+}
+function sumSalaries(nambers) {
+   var resalt = 0
+ for (var namber of Object.values(nambers)) {
+    resalt = resalt + namber
+ }
+ return resalt
+}
+alert(sumSalaries(salaries))
+console.log(sumSalaries(salaries)) // Natija: 8200 -> chiqishi kk
